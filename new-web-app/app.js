5412542ab5a939a99d0cacdb25f8e62bda64fcad
gowoonk <40eee@naver.com>
2025-09-26 16:14:31 +0900

    Fix: 로그인시 발생하는 로그 수정

---
+++ new-web-app/app.js
@@ -0,0 +1,8 @@
+function login(username, password) {
+    if(username !== "" && password !== ""){
+        console.log("login successful");
+    }
+    else{
+        console.log("login failed");
+    }
+}
